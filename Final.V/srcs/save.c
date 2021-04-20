58d34b313fb2af3ae5893a5d32304986bf628da2
hesayah <hedi.hsayah@gmail.com>
2021-04-20 03:03:56 +0200

    --save fixed

--- Final.V/srcs/save.c
+++ Final.V/srcs/save.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   save.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hesayah <hesayah@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hedi <hedi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 15:07:39 by hesayah           #+#    #+#             */
-/*   Updated: 2021/04/18 18:11:28 by hesayah          ###   ########.fr       */
+/*   Updated: 2021/04/20 02:50:50 by hedi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,24 +34,22 @@ void	ft_header_bmp(int fd, t_data *data)
   int	tmp;
 
   write(fd, "BM", 2);
-
-  tmp = 14 + 40 + 4 * data->w_w * data->w_h; //La taille totale du fichier en octets (codée sur 4 octets)
+  tmp = 54 + 4 * data->w_w * data->w_h; 
   write(fd, &tmp, 4);
   tmp = 0;
   write(fd, &tmp, 2); 
-  write(fd, &tmp, 2); 
+  write(fd, &tmp, 2);
   tmp = 54;
   write(fd, &tmp, 4);
   tmp = 40;
   write(fd, &tmp, 4);
-  write(fd, &data->w_w, 4); //La largeur de l'image (sur 4 octets), c'est-à-dire le nombre de pixels horizontalement (en anglais width)
-  write(fd, &data->w_h, 4); //La hauteur de l'image (sur 4 octets), c'est-à-dire le nombre de pixels verticalement (en anglais height)
+  write(fd, &data->w_w, 4);
+  write(fd, &data->w_h, 4);
   tmp = 1;
-  write(fd, &tmp, 2); //Le nombre de plans (sur 2 octets). Cette valeur vaut toujours 1
-  write(fd, &data->bits_per_pixel, 2); //La profondeur de codage de la couleur(sur 2 octets), c'est-à-dire le nombre de bits utilisés 
-  		    		           //pour coder la couleur. Cette valeur peut-être égale à 1, 4, 8, 16, 24 ou 32
+  write(fd, &tmp, 2);
+  write(fd, &data->bits_per_pixel, 2);
   tmp = 0;
-  write(fd, &tmp, 4); //La méthode de compression (sur 4 octets). Cette valeur vaut 0 lorsque l'image n'est pas compressée
+  write(fd, &tmp, 4);
   write(fd, &tmp, 4);
   write(fd, &tmp, 4);
   write(fd, &tmp, 4);
@@ -59,21 +57,20 @@ void	ft_header_bmp(int fd, t_data *data)
   write(fd, &tmp, 4);
 }
 
-void    create_img_bmp(int fd, int *img, t_data *data)
+void    create_save_bmp(int fd, t_data *data)
 {
   int x;
   int y;
 
   
-  y = data->w_h;
-  while (y > 0)
+  y = data->w_h - 1;
+  while (y >= 0)
   {
     x = 0;
     while (x < data->w_w)
     {
-        data->srt.color = img[data->w_h * y + x];
-        write(fd, &data->srt.color, 16);
-        x++;
+      write(fd, &data->addr[y * data->line_length + x * (data->bits_per_pixel / 8)], 4);
+      x++;
     }
     y--;
   }
@@ -94,15 +91,17 @@ void	save_frame(t_data *data)
 
 void    save(t_data  *data)
 {
-	FILE *save_img;
+	int fd;
 
-	save_img = NULL;
-    data->img = mlx_new_image(data->mlx, data->w_w, data->w_h);
+  fd = open("save.bmp", O_RDONLY) ;
+  if (fd != -1)
+    system("rm save.bmp");
+	fd = open("save.bmp", O_CREAT | O_WRONLY, 0777);
+  data->img = mlx_new_image(data->mlx, data->w_w, data->w_h);
 	data->addr = mlx_get_data_addr(data->img, &data->bits_per_pixel,
 	&data->line_length, &data->endian);
-    save_frame(data);
-	fopen("../save.bmp", "w");
-    ft_header_bmp(save_img, data);
-    create_img_bmp(save_img, data->img, data);
-	fclose(save_img);
+  save_frame(data);
+  ft_header_bmp(fd, data);
+  create_save_bmp(fd, data);
+	close(fd);
 }
